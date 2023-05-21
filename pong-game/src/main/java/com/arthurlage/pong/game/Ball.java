84812ac0e0d6d2e56013f045a70999c9c538a282
Arthur Lage <arthurlage2006@gmail.com>
2023-05-21 18:12:32 -0300

    chore: Create base for Ball class

---
+++ pong-game/src/main/java/com/arthurlage/pong/game/Ball.java
@@ -0,0 +1,28 @@
+package com.arthurlage.pong.game;
+
+import java.awt.Rectangle;
+import java.util.Random;
+
+public class Ball extends Rectangle {
+  Random random;
+  int xVelocity;
+  int yVelocity;
+
+  Ball() {
+
+  }
+
+  public void setXDirection(int randomXDirection) {
+  }
+
+  public void setYDirection(int randomYDirection) {
+  }
+
+  public void move() {
+
+  }
+
+  public void draw() {
+
+  }
+}
