471463b9f1f12d4265fc0c6ce981f8cab5e56d2d
Arthur Lage <arthurlage2006@gmail.com>
2023-05-21 18:12:27 -0300

    chore: Create base for Score class

---
+++ pong-game/src/main/java/com/arthurlage/pong/game/Score.java
@@ -0,0 +1,19 @@
+package com.arthurlage.pong.game;
+
+import java.awt.Graphics;
+import java.awt.Rectangle;
+
+public class Score extends Rectangle {
+  static int GAME_WIDTH;
+  static int GAME_HEIGHT;
+  int player1;
+  int player2;
+
+  Score() {
+
+  }
+
+  public void draw(Graphics g) {
+
+  }
+}
