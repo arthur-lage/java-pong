9f55e815ebe3a132d123aeb8bde5b99c15a2eff9
Arthur Lage <arthurlage2006@gmail.com>
2023-05-21 18:13:50 -0300

    chore: Create base for App

---
+++ pong-game/src/main/java/com/arthurlage/pong/App.java
@@ -0,0 +1,9 @@
+package com.arthurlage.pong;
+
+import com.arthurlage.pong.game.GameFrame;
+
+public class App {
+    public static void main(String[] args) {
+        GameFrame gameFrame = new GameFrame();
+    }
+}
