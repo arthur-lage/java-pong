4f738700ce847b55a9cab43170f5dcb56eadda72
Arthur Lage <arthurlage2006@gmail.com>
2023-05-21 18:13:29 -0300

    chore: Create base for Paddle class

---
+++ pong-game/src/main/java/com/arthurlage/pong/game/Paddle.java
@@ -0,0 +1,33 @@
+package com.arthurlage.pong.game;
+
+import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+
+public class Paddle {
+  int id;
+  int yVelocity;
+
+  Paddle() {
+
+  }
+
+  public void keyPressed(KeyEvent e) {
+
+  }
+
+  public void keyReleased(KeyEvent e) {
+
+  }
+
+  public void setYDirection(int yDirection) {
+
+  }
+
+  public void move() {
+
+  }
+
+  public void draw(Graphics g) {
+
+  }
+}
